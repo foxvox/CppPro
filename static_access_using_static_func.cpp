a697da1f96fd10f1ba25e84ccf48879f8a417644
alex <windev@nate.com>
2024-12-19 07:37:10 +0900

    function pointer

---
+++ static_access_using_static_func.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>  
+using namespace std;
+
+void TelepathicFunction(); 
+
+class Cat
+{
+public:
+	Cat(int age) 
+		: itsAge(age) 
+	{ HowManyCats++; }
+
+	virtual ~Cat() 
+	{ HowManyCats--; }
+
+	virtual int GetAge() { return itsAge; }
+	virtual void SetAge(int age) { itsAge = age; }
+	
+	static int GetHowMany() { return HowManyCats; } 	
+
+private:
+	int itsAge;
+	static int HowManyCats;
+};
+
+int Cat::HowManyCats = 0; 
+
+void TelepathicFunction() 
+{
+	cout << "There are " << Cat::GetHowMany() << " cats alive!\n";  
+}
+
+int main()
+{
+	const int MaxCats = 5; int i;
+	Cat* CatHouse[MaxCats];
+
+	for (i = 0; i < MaxCats; i++)
+	{
+		CatHouse[i] = new Cat(i);
+		TelepathicFunction(); 
+	}
+
+	for (i = 0; i < MaxCats; i++)
+	{
+		delete CatHouse[i];
+		TelepathicFunction(); 		
+	}
+
+	return 0;
+}
+
+
