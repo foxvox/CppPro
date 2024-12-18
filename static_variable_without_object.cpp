244fb21faccaa3db46ccc7289f0b8712c7485b85
alex <windev@nate.com>
2024-12-19 06:59:38 +0900

    static access using nonstatic func

--- static_variable_without_object.cpp
+++ static_variable_without_object.cpp
@@ -8,40 +8,39 @@ public:
 	virtual ~Cat() { HowManyCats--; }
 	virtual int GetAge() { return itsAge; }
 	virtual void SetAge(int age) { itsAge = age; }
-	virtual int GetHowMany() { return HowManyCats; } 	
+
+	static int HowManyCats; 
 
 private:
-	int itsAge;
-	static int HowManyCats;
+	int itsAge; 
 };
 
 int Cat::HowManyCats = 0; 
 
-void TelepathicFunction(Cat* pCat) 
+void TelepathicFunction()
 {
 	cout << "There are ";
 	// 객체 없이 static 멤버변수에 접근 
-	cout << pCat->GetHowMany() << " cats left!\n";
-	cout << "Deleting the one which is "; 
-	cout << pCat->GetAge() + 2 << " years old\n"; 
+	cout << Cat::HowManyCats << " cats alive!\n";
 }
 
 int main()
 {
-	const int MaxCats = 5; int i;
-	Cat* CatHouse[MaxCats];
+	const int MaxCats = 5; int i; 
+	Cat* CatHouse[MaxCats]; 
 	for (i = 0; i < MaxCats; i++)
 	{
-		CatHouse[i] = new Cat(i);
+		CatHouse[i] = new Cat(i);  
+		TelepathicFunction(); 
 	}
 
 	for (i = 0; i < MaxCats; i++)
 	{
-		TelepathicFunction(CatHouse[i]); 
-		delete CatHouse[i];		
+		delete CatHouse[i]; 
+		TelepathicFunction(); 
 	}
 
-	return 0;
+	return 0; 
 }
 
 
